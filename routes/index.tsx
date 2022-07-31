792b81fb85c24b43cd6e88389b507fe9cbdd04c5
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-31 15:26:44 +0900

    Initial commit.

---
+++ routes/index.tsx
@@ -0,0 +1,22 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p class={tw`my-6`}>
+        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+        file, and refresh.
+      </p>
+      <Counter start={3} />
+      <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2022/07/30/095030?_ga=2.78326934.324356107.1659107296-905746074.1658639894" target="_blank" rel="noreferrer">DenoのWebフレームワーク Fresh(deno-fresh)</a><br />
+    </div>
+  );
+}
